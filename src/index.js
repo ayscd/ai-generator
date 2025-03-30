c90f94053cbcb2459bee5901c5ce2f9b73036c36
ayscd <165339629+ayscd@users.noreply.github.com>
2025-03-30 11:49:58 -0300

    Update 5 - linking the AI API
    - connecting the file to the API
    - interpolating the inputs with the instructions
    - setting up the context and the prompt

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+function displayPoem(poem) {
+    new Typewriter('#poem', {
+    strings: poem.data.answer,
+    autoStart: true,
+    delay: 45,
+    cursor: "",
+    pauseFor: 1000,
+  });
+}
+
 function generatePoem(event) {
     event.preventDefault();
 
-    new Typewriter('#poem', {
-      strings: `Batatinha quando nasce`,
-      autoStart: true,
-      delay: 45,
-      cursor: "",
-      pauseFor: 1000,
-    });
+    let poemInput = document.querySelector("#poem-input");
+    let apiKey = "do8ae0f5a33a8b61b41f1t1ed44678b4";
+    let prompt = `User instructions: Generate a poem in Portuguese (PT-BR) about the ${poemInput.value}`;
+    let context = "You are a romantic poem expert, and like to write short poems. Your mission is to generate 4 line poems, and separate each line with <br />. Make sure to follow the user instructions. Do not include a title to the poem. At the end, sign the poem with 'SheCodes AI' inside a <strong> element";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+    let poem = document.querySelector("#poem");
+    poem.classList.remove("hidden");
+    poem.innerHTML = `<div class="generating-poem"> ⏳ Generating a poem about ${poemInput.value}...</div>`;
+    
+    axios.get(apiUrl).then(displayPoem);
 }
 
 
